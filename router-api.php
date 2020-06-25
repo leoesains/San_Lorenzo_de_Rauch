10a215f195ec66e50dfc396d4a7e538fd52a2670
leo esains <leo.esains@gmail.com>
2020-06-25 09:29:09 -0300

    Arregle error en api.router

--- router-api.php
+++ router-api.php
@@ -1,12 +1,14 @@
 <?php
 
 require_once 'libs/router/Router.php';
+require_once 'api/comments-api.controller.php';
 
 //Creo el ruteador usando la libreria externa que tengo guarda en la carpeta libs
 $router = new Router();
 
 //Creo la tabla de ruteo
 $router->addRoute('comentarios/:ID', 'GET', 'CommentsApiController', 'getComments'); //obtengo todos los comentarios de un jugador dado
+
 
 //Rutea
 $router->route($_REQUEST['resource'], $_SERVER['REQUEST_METHOD']);       //$_REQUEST['resource']: recurso del usuario, $_SERVER['REQUEST_METHOD']: verbo
